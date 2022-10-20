e70612cbda9d589a096270b487ccd8d59a6f4331
Nir Roiter <aniroiter@gmail.com>
2022-10-20 12:20:17 +0300

    Revised

---
+++ js/books_controler.js
@@ -0,0 +1,241 @@
+'use strict'
+
+function onInit() {
+  renderFilterByQueryStringParams()
+  loadUserPref()
+  renderBooks()
+  doTrans()
+}
+
+function renderBooks() {
+  gViewType === 'table' ? renderTable() : renderCards()
+}
+
+function renderCards() {
+  pageSize = 5
+  var books = getBooks()
+  var strHtml = books.map(
+    (book) => `
+  <article class="book-preview">
+      <button title="Remove Book" class="btn-remove" onclick="onRemoveBook('${
+        book.id
+      }')">X</button>
+      <img onerror="this.src='img/no-img.png'" src="${book.imgUrl}" alt="">
+      <h5>${book.name}</h5>
+      <h6><span data-trans="price">Price </span> <span> ${formatCurrency(
+        book.price
+      )}</span></h6>
+      <section calss="btn-action">
+      <button  onclick="onReadBook('${
+        book.id
+      }')"><span data-trans="details" class="card-btn">Details</span></button>
+      <button data-trans="update" class="card-btn" onclick="onUpdateBook('${
+        book.id
+      }')" >Update</button>
+      </section>
+  </article>
+  `
+  )
+  document.querySelector('.books-container').innerHTML = strHtml.join('')
+  doTrans()
+}
+
+function renderTable() {
+  pageSize = 10
+  var books = getBooks()
+  var strHtml = books.map(
+    (book) => `
+  <tr>
+  <td>${book.id}</td><td>${book.name}</td><td>${book.price}</td><td class="action-buttons"><button data-trans="details" class="read" 
+  onclick="onReadBook('${book.id}')">Read</button>
+  <button data-trans="update" class="update" onclick="onUpdateBook('${book.id}')">Update</button>
+  <button data-trans="delete" class="delete" onclick="onRemoveBook('${book.id}')">Delete</button>
+      </td>
+</tr> `
+  )
+  document.querySelector('.books-container').innerHTML =
+    `<table>
+  <th>Id</th>
+  <th title="Sort by name" class="sort-by title" data-trans="book-title" onclick="onSetSortBy('name')">Title</th>
+  <th title="Sort by price"class="sort-by price" data-trans="price" onclick="onSetSortBy('price')">Price</th>
+  <th class="action-buttons" data-trans="actions">Actions</th>
+  <tbody class="books-table">` + strHtml.join('')
+  doTrans()
+}
+
+function onRemoveBook(bookId) {
+  removeBook(bookId)
+  renderBooks()
+}
+
+function onAddBook() {
+  renderAddBookModal()
+  document.querySelector('.actions-form').style.display = 'block'
+}
+
+function onUpdateBook(bookId) {
+  gCurrBookId = bookId
+  renderUpdateModal()
+  document.querySelector('.actions-form').style.display = 'block'
+}
+
+function onReadBook(bookId) {
+  var book = getBookById(bookId)
+  gCurrBookId = bookId //  add a function to set the global variable value
+  var elModal = document.querySelector('.modal')
+  elModal.querySelector('h3').innerText = book.name
+  elModal.querySelector('h4 span').innerText = book.price
+  elModal.querySelector('p').innerText = makeLorem()
+  elModal.querySelector('.num').innerText = book.rate
+  elModal.querySelector('.modal-img').src = book.imgUrl
+  elModal.classList.add('open')
+}
+
+function onCloseModal() {
+  document.querySelector('.modal').classList.remove('open')
+}
+
+function onBookRate(val) {
+  // change the val to numerical value and rename
+  bookRate(val)
+  document.querySelector('.num').innerText = getBookById(gCurrBookId).rate
+  renderBooks()
+}
+
+function onSetFilterBy(filterBy) {
+  filterBy = setBookFilter(filterBy)
+  renderBooks()
+
+  const queryStringParams = `?price=${filterBy.maxPrice}&minRate=${filterBy.minRate}&name=${filterBy.txt}`
+  const newUrl =
+    window.location.protocol +
+    '//' +
+    window.location.host +
+    window.location.pathname +
+    queryStringParams
+  window.history.pushState({ path: newUrl }, '', newUrl)
+}
+
+function renderFilterByQueryStringParams() {
+  const queryStringParams = new URLSearchParams(window.location.search)
+  const filterBy = {
+    minRate: +queryStringParams.get('minRate') || 0,
+    maxPrice: +queryStringParams.get('price') || 31,
+    txt: queryStringParams.get('name') || '',
+  }
+
+  if (!filterBy.minRate && !filterBy.maxPrice && !filterBy.txt) return
+
+  document.querySelector('.filter-max-price').value = filterBy.maxPrice
+  document.querySelector('.filter-min-rate').value = filterBy.minRate
+  document.querySelector('.name-search').value = filterBy.txt
+  setBookFilter(filterBy)
+}
+
+function onSetSortBy(prop) {
+  isDesc = !isDesc
+  const sortBy = {
+    [prop]: isDesc ? -1 : 1,
+  }
+  setBookSort(sortBy)
+  renderBooks()
+}
+
+function onNextPage() {
+  nextPage()
+  renderBooks()
+}
+
+function onSetView(viewType) {
+  setView(viewType)
+  renderBooks()
+}
+
+function renderAddBookModal() {
+  document.querySelector('.actions-form').innerHTML = ''
+  var strHtml = `
+ 
+  <form class="add-book-modal">
+  <h2 data-trans="add-book-title">Adding New Book Form</h2>
+        <div class="container">
+        <button title="Close" class="btn-remove" onclick="onCloseActionsModal()">X</button>
+            <label><b data-trans="name">Book name</b></label>
+            <input type="text" placeholder="Enter Book Name" data-trans="add-book-name" name="book-name" required>
+
+            <label><b data-trans="price">Price</b></label>
+            <input type="number"  placeholder="Enter Price" data-trans="price" name="book-price" required>
+            <br>
+            <label><b data-trans="book-cover">Book Cover</b></label>
+            <input type="text" placeholder="Paste img URL" data-trans="update-img-ph" name="imgURL" >
+                   <div class="clearfix">
+                <button data-trans="cancel" type="button" onclick="onCloseActionsModal()" class="cancel-btn">Cancel</button>
+                <button data-trans="update" type="submit" class="form-submit-btn" onclick="onSubmitBook(event)"></button>
+            </div>
+        </div>
+    </form>
+</div>
+`
+  document.querySelector('.actions-form').innerHTML = strHtml
+  doTrans()
+}
+
+function renderUpdateModal() {
+  document.querySelector('.actions-form').innerHTML = ''
+  var strHtml = `
+ 
+<h2 data-trans="update-modal-title">Updating Book Info</h2>
+    <form class="modal-content">
+        <div class="container">
+              <label><b>Price</b></label>
+            <input type="number" placeholder="Enter New Price" data-trans="price" name="book-price" required>
+            <label><b>Book Cover</b></label>
+            <input type="text" placeholder="Update Book img URL" data-trans="update-img-ph" name="imgURL" >
+                   <div class="clearfix">
+                <button type="button" onclick="onCloseActionsModal()" class="cancel-btn" data-trans="cancel">Cancel</button>
+                <button type="submit" class="form-submit-btn" onclick="onSubmitUpdate(event)" data-trans="update">Update Details</button>
+            </div>
+        </div>
+    </form>
+</div>
+`
+  document.querySelector('.actions-form').innerHTML = strHtml
+  doTrans()
+}
+
+function onCloseActionsModal() {
+  var modal = document.querySelector('.actions-form')
+  modal.style.display = 'none'
+}
+
+function onSubmitBook(ev) {
+  ev.preventDefault()
+  var bookName = document.querySelector('[name="book-name"]').value
+  var bookPrice = document.querySelector('[name="book-price"]').value
+  var imgURL = document.querySelector('[name="imgURL"]').value
+  if (bookName === '') return
+  addBook(bookName, bookPrice, imgURL)
+  onCloseActionsModal()
+  renderBooks()
+}
+
+function onSubmitUpdate(ev) {
+  ev.preventDefault()
+  var bookPrice = document.querySelector('[name="book-price"]').value
+  var imgURL = document.querySelector('[name="imgURL"]').value
+  if (!bookPrice && !imgURL) return
+  updateBook(gCurrBookId, bookPrice, imgURL)
+  onCloseActionsModal()
+  renderBooks()
+}
+
+function onSetLang(lang) {
+  setLang(lang)
+  setDirection(lang)
+  doTrans()
+  renderBooks()
+}
+
+function setDirection(lang) {
+  if (lang === 'he') document.body.classList.add('rtl')
+  else document.body.classList.remove('rtl')
+}
---
+++ js/services/i18nservice.js
@@ -0,0 +1,179 @@
+'use strict'
+const gTrans = {
+  title: {
+    en: 'Welcome to our bookshop',
+    he: 'תשומת ספרים',
+  },
+  'lagn-selctor': {
+    en: 'Select language : ',
+    he: ' בחר שפה :',
+  },
+  'filter-title': {
+    en: 'Filter your books',
+    he: 'סנן ספרים',
+  },
+  'filter-price-title': {
+    en: 'Max Price:',
+    he: 'מחיר מקסימלי:',
+  },
+  'filter-rate-title': {
+    en: 'Minimum Rate:',
+    he: 'דרוג מינימלי:',
+  },
+  'search-placeholder': {
+    en: 'Search by name...',
+    he: 'חפש לפי שם...',
+  },
+  'sort-by-name': {
+    en: 'Sort by Name',
+    he: 'מיין לפי שם',
+  },
+  'sort-by-price': {
+    en: 'Sort by Price',
+    he: 'מיין לפי מחיר',
+  },
+  'add-book': {
+    en: 'Add new book',
+    he: 'הוסף ספר חדש',
+  },
+  'next-page': {
+    en: 'Next page',
+    he: 'לדף הבא',
+  },
+  price: {
+    en: 'Price',
+    he: 'מחיר',
+  },
+  'modal-desc': {
+    en: 'Book Description',
+    he: 'על הספר',
+  },
+  'modal-rate': {
+    en: 'Rate this Book:',
+    he: 'דרג את הספר',
+  },
+  'modal-close': {
+    en: 'Close',
+    he: 'סגור',
+  },
+  'modal-price': {
+    en: 'Price',
+    he: 'מחיר',
+  },
+  update: {
+    en: 'Update',
+    he: 'עדכן',
+  },
+  details: {
+    en: 'Details',
+    he: 'פרטים',
+  },
+  delete: {
+    en: 'Delete',
+    he: 'מחק',
+  },
+  'book-title': {
+    en: 'Title',
+    he: 'שם הספר',
+  },
+  actions: {
+    en: 'Actions',
+    he: 'פעולות',
+  },
+  'add-book-title': {
+    en: 'Adding New Book Form',
+    he: 'טופס הוספת ספר',
+  },
+  name: {
+    en: 'Name',
+    he: 'שם הספר',
+  },
+  'book-cover': {
+    en: 'Book Cover',
+    he: 'תמונת הכריכה',
+  },
+  cancel: {
+    en: 'Cancel',
+    he: 'ביטול',
+  },
+  'update-img-ph': {
+    en: 'Update Book img URL',
+    he: 'קישור לתמונת הספר',
+  },
+  'update-modal-title': {
+    en: 'Updating Book Info',
+    he: 'עדכן את פרטי הספר',
+  },
+  'add-book-name': {
+    en: 'Enter Book Name',
+    he: 'הכנס את שם הספר',
+  },
+}
+
+let gCurrLang = 'en'
+
+const gCurrencies = {
+  en: {
+    format: 'en-US',
+    curSign: 'USD',
+  },
+  he: {
+    format: 'he-IL',
+    curSign: 'ILS',
+  },
+}
+
+function getTrans(transKey) {
+  const transMap = gTrans[transKey]
+  if (!transMap) return 'UNKNOWN'
+
+  let trans = transMap[gCurrLang]
+  if (!trans) trans = transMap.en
+  return trans
+}
+
+function doTrans() {
+  const els = document.querySelectorAll('[data-trans]')
+  els.forEach((el) => {
+    const transKey = el.dataset.trans
+    const trans = getTrans(transKey)
+    el.innerText = trans
+    if (el.placeholder) el.placeholder = trans
+  })
+}
+
+function setLang(lang) {
+  gCurrLang = lang
+}
+
+function formatNum(num) {
+  return new Intl.NumberFormat(gCurrLang).format(num)
+}
+
+function formatDate(time) {
+  const options = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  }
+  return new Intl.DateTimeFormat(gCurrLang, options).format(time)
+}
+
+function getCurrency() {
+  const currencyMap = gCurrencies
+  if (!currencyMap) return 'UNKNOWN'
+  let currency = currencyMap[gCurrLang]
+  if (!currency) currency = currencyMap.en
+  return currency
+}
+
+function formatCurrency(price) {
+  const currency = getCurrency()
+  return new Intl.NumberFormat(currency.format, {
+    style: 'currency',
+    currency: currency.curSign,
+  }).format(price)
+}
